aa9e7d4dd1370ff5fcf0bfbd986d9c6c8fb02e4c
joe shin <joe.shin@live.com>
2016-07-02 11:49:03 -0400

    added node command lines

---
+++ codesnippets.js
@@ -0,0 +1,12 @@
+//takes all the data written
+	process.argv.splice(2, process.argv.length-1);
+
+//makes the json file pretty
+	null, 2
+
+//if there is no error, pring a data json format
+	if(!err){
+	console.log(JSON.stringify(data));
+
+// Take in the command line arguments
+	var anything = process.argv;
