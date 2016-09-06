9b46780e293956f2d14464b2103835a4ec3514b1
joe shin <joe.shin@live.com>
2016-09-06 19:35:49 -0400

    added ExtraJavascript

--- ExtraJavascript.js
+++ ExtraJavascript.js
@@ -11,3 +11,9 @@ var ids = items.map(function(item) {
 var cheapItems = items.filter(function(item) {
     return item.price <= 20;    
 });
+
+items.map(function(item) {
+    return item.price
+}).reduce(function(previous, current) {
+    return previous + current;
+});
