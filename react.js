4b35d816f2a240cb05cc8fc2342444d919d6b2f7
joe shin <joe.shin@live.com>
2016-10-30 23:02:48 -0400

    embedding expressions in jsx

--- react.js
+++ react.js
@@ -89,5 +89,27 @@ props is the name of the object that stores passed-in information. this.props re
 
 
 
+Embedding Expressions in JSX
+You can embed any JavaScript expression in JSX by wrapping it in curly braces.
 
+For example, 2 + 2, user.name, and formatName(user) are all valid expressions:
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 
+const user = {
+	
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
