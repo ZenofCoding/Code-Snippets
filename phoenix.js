134ee473b446c20cbac45383939f72ed0903a8d5
joe shin <joe.shin@live.com>
2016-10-31 23:56:42 -0400

    phoenix

---
+++ phoenix.js
@@ -0,0 +1,5 @@
+Phoenix.HTML- functions to handle HTML safety;
+Phoenix.HTML.Tag - functions for generating HTML tags;
+Phoenix.HTML.Form - functions for working with forms;
+Phoenix.HTML.Link - functions for generating links and urls;
+Phoenix.HTML.Format - functions for formatting text;
