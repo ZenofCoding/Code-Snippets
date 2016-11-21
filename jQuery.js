7c705ea558146286e70a65ad82c99a84157c290a
joe shin <joe.shin@live.com>
2016-11-20 23:01:10 -0500

    document.ready

---
+++ jQuery.js
@@ -0,0 +1,61 @@
+----------------------------------
+What is jQuery?
+----------------------------------
+jQuery is a library, or set of helpful add-ons, to the JavaScript programming language. 
+
+//-------------------------------//
+
+
+
+-----------------------------------
+Linking Script File
+-----------------------------------
+/*
+<script type="text/javascript" src="script.js"></script>
+
+*/
+
+//-------------------------------//
+
+
+-----------------------------------
+Setting up
+-----------------------------------
+
+$(document).ready(function(){});
+
+//-------------------------------//
+
+
+-----------------------------------
+Functions, Part I: $(document).ready
+-----------------------------------
+
+$(document) is a jQuery object. 
+The $() is actually a function in disguise; it turns the document into a jQuery object.
+
+.ready() is a type of function; you can think of it as sort of a helper that runs the 
+code inside its parentheses as soon as the HTML document is ready.
+
+function(){} is the action that .ready() will perform as soon as the HTML document is loaded.
+ (In the above example, the Do something placeholder is where those actions would go.)
+//-------------------------------//
+
+-----------------------------------
+
+-----------------------------------
+
+
+//-------------------------------//
+-----------------------------------
+Functions, Part I: $(document).ready
+-----------------------------------
+
+$(document) is a jQuery object. 
+The $() is actually a function in disguise; it turns the document into a jQuery object.
+
+.ready() is a type of function; you can think of it as sort of a helper that runs the 
+code inside its parentheses as soon as the HTML document is ready.
+
+function(){} is the action that .ready() will perform as soon as the HTML document is loaded.
+ (In the above example, the Do something placeholder is where those actions would go.)
