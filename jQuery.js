6d9df10cced34f5f9be0ef0eab732e99649769f8
joe shin <joe.shin@live.com>
2016-11-25 23:04:13 -0500

    deleted duplicate

--- jQuery.js
+++ jQuery.js
@@ -47,15 +47,3 @@ function(){} is the action that .ready() will perform as soon as the HTML docume
 
 
 //-------------------------------//
------------------------------------
-Functions, Part I: $(document).ready
------------------------------------
-
-$(document) is a jQuery object. 
-The $() is actually a function in disguise; it turns the document into a jQuery object.
-
-.ready() is a type of function; you can think of it as sort of a helper that runs the 
-code inside its parentheses as soon as the HTML document is ready.
-
-function(){} is the action that .ready() will perform as soon as the HTML document is loaded.
- (In the above example, the Do something placeholder is where those actions would go.)
